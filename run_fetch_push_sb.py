d4f08a2e35bcf1c7fc204eb197bca902ea60a22b
Paul Gajewski <lubiluk@gmail.com>
2021-04-19 12:39:15 +0200

    no hand, just fetch

--- run_fetch_push_sb.py
+++ run_fetch_push_sb.py
@@ -1,15 +1,18 @@
 import gym
 import gym_fetch
 
-from stable_baselines3 import HER, SAC
+from stable_baselines3 import HER
 from stable_baselines3.sac import MlpPolicy
+from sb3_contrib.common.wrappers.time_feature import TimeFeatureWrapper
 
 env = gym.make("FetchPush-v2")
+
+env = TimeFeatureWrapper(gym.make('FetchPush-v2'))
 
 model = HER.load("data/fetch_push_sb", env=env)
 
 obs = env.reset()
-for _ in range(100):
+for _ in range(1000):
     action, _ = model.predict(obs, deterministic=True)
     obs, reward, done, _ = env.step(action)
     env.render()
